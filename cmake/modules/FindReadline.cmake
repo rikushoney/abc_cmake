03533910dc11db3938150cddfe7b4e76a5abea29
Rikus Honey <rikushoney2@gmail.com>
2023-10-26 01:49:30 +0200

    make cmake happy by not importing the same library twice

--- cmake/modules/FindReadline.cmake
+++ cmake/modules/FindReadline.cmake
@@ -2,8 +2,12 @@ include(FindPackageHandleStandardArgs)
 
 set(Readline_FOUND false)
 
-find_library(Readline_LIBRARY NAMES readline)
-find_path(Readline_INCLUDE_DIR NAMES readline/readline.h)
+if(NOT Readline_LIBRARY)
+  find_library(Readline_LIBRARY NAMES readline)
+endif()
+if(NOT Readline_INCLUDE_DIR)
+  find_path(Readline_INCLUDE_DIR NAMES readline/readline.h)
+endif()
 
 find_package_handle_standard_args(Readline
   REQUIRED_VARS
@@ -11,16 +15,19 @@ find_package_handle_standard_args(Readline
   Readline_INCLUDE_DIR
 )
 
-if (Readline_FOUND)
-  mark_as_advanced(Readline_LIBRARY)
-  
-  add_library(Readline::Readline UNKNOWN IMPORTED)
-  set_property(TARGET Readline::Readline
-    PROPERTY
-    IMPORTED_LOCATION ${Readline_LIBRARY}
-  )
-  target_include_directories(Readline::Readline
-    INTERFACE
-    ${Readline_INCLUDE_DIR}
-  )
+if(Readline_FOUND)
+  if(NOT TARGET Readline::Readline)
+    add_library(Readline::Readline UNKNOWN IMPORTED)
+    set_property(TARGET Readline::Readline
+      PROPERTY
+      IMPORTED_LOCATION ${Readline_LIBRARY}
+    )
+    target_include_directories(Readline::Readline
+      INTERFACE
+      ${Readline_INCLUDE_DIR}
+    )
+  endif()
 endif()
+
+mark_as_advanced(Readline_LIBRARY)
+mark_as_advanced(Readline_INCLUDE_DIR)
